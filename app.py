ddca2a4082cc7e96dd6a57de43f1a540f95c5b34
jefjvincent <43353689+jefjvincent@users.noreply.github.com>
2020-05-15 20:17:11 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from libs.openexchange import OpenExchangeClient
+import tkinter as tk
+from tkinter import ttk
+APP_ID="569a4c3347f940909e5343bdd822f93d"
+
+def conversion():
+    client = OpenExchangeClient(APP_ID)
+    result.set(str(format(float(client.convert(float(amount.get() or 1), "CAD", "INR")),'.2f'))+' INR')
+
+root = tk.Tk()
+root.title("Exchange Rates")
+
+amount = tk.StringVar()
+result = tk.StringVar()
+
+top_frame = tk.Frame(root).pack(side="left", fill="both", expand=True)
+amount_label = ttk.Label(top_frame, text=" Amount in CAD: ").pack(side="left", padx=(0, 10))
+amount_entry = ttk.Entry(top_frame, width=15, textvariable=amount)
+amount_entry.pack(side="left")
+amount_entry.focus()
+convert_button = ttk.Button(top_frame, text="Calculate", command=conversion).pack(side="left", fill="x", expand=True)
+
+res_frame = tk.Frame(root).pack(side="left", fill="both", expand=True)
+result_label = ttk.Label(res_frame, textvariable=result).pack(side="left", padx=(0, 10))
+
+root.mainloop()
+
+
